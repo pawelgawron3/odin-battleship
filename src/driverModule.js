f317b17f134e2afc2e7a9f4aab12f95112f600bc
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-02-15 14:00:23 +0100

    Import createBoard() function

--- src/driverModule.js
+++ src/driverModule.js
@@ -1,1 +1,10 @@
 import { Player } from './classes/player.js';
+import { createBoard } from './helperFunctions/generateBoard.js';
+
+const userGrid = document.getElementById('userGrid');
+const computerGrid = document.getElementById('computerGrid');
+
+export function driverModule() {
+  createBoard(userGrid);
+  createBoard(computerGrid);
+}
