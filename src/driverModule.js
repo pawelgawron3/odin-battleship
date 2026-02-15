59f5ba491f0a8b1ea02b8e1734ea38ff1ea21b5d
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-02-15 17:44:04 +0100

    Update the file

--- src/driverModule.js
+++ src/driverModule.js
@@ -1,15 +1,19 @@
 import { createBoard } from './helperFunctions/createBoard.js';
 import { handleUserPlacement } from './helperFunctions/handleUserPlacement.js';
 import { GameController } from './classes/gameController.js';
+import { handlePlayerAttack } from './helperFunctions/handlePlayerAttack.js';
+import { placeComputerShips } from './helperFunctions/placeComputerShips.js';
 
 const rotateShipBtn = document.querySelector('#rotateShip');
 const userGrid = document.getElementById('userGrid');
 const computerGrid = document.getElementById('computerGrid');
 
 let gameController = new GameController();
+placeComputerShips(gameController);
 
 userGrid.addEventListener('click', (e) => handleUserPlacement(e, gameController));
 rotateShipBtn.addEventListener('click', () => gameController.rotateShip());
+computerGrid.addEventListener('click', (e) => handlePlayerAttack(e, gameController, computerGrid));
 
 export function driverModule() {
   createBoard(userGrid);
