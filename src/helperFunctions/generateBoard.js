e7baa32901a670d8019618298de77231c61d3d09
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-02-15 13:57:45 +0100

    Create generateBoard.js

---
+++ src/helperFunctions/generateBoard.js
@@ -0,0 +1,12 @@
+function createBoard(gridElement) {
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.dataset.index = [i, j];
+      gridElement.appendChild(cell);
+    }
+  }
+}
+
+export { createBoard };
