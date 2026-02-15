84b9cb66dfcd6498c2261438281d6c4d62cea71c
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-02-15 15:55:30 +0100

    Create GameController class

---
+++ src/classes/gameController.js
@@ -0,0 +1,44 @@
+import { Player } from './player.js';
+import { Ship } from './ship.js';
+
+class GameController {
+  constructor() {
+    this.user = new Player('user');
+    this.computer = new Player('computer');
+    this.currentTurn = 'user';
+
+    this.currentDirection = 'horizontally';
+    this.shipsToPlace = [5, 4, 3, 3, 2, 2, 1];
+    this.currentShipIndex = 0;
+  }
+
+  get currentShipLength() {
+    return this.shipsToPlace[this.currentShipIndex];
+  }
+
+  placeUserShip(row, col) {
+    const length = this.currentShipLength;
+    if (!length) return false;
+
+    const ship = new Ship(length);
+    if (this.user.gameboard.canPlaceShip(ship, row, col, this.currentDirection)) {
+      this.user.gameboard.placeShip(ship, [row, col], this.currentDirection);
+
+      this.currentShipIndex++;
+      return true;
+    }
+
+    return false;
+  }
+
+  rotateShip() {
+    this.currentDirection =
+      this.currentDirection === 'horizontally' ? 'vertically' : 'horizontally';
+  }
+
+  allShipsPlaced() {
+    return this.currentShipIndex >= this.shipsToPlace.length;
+  }
+}
+
+export { GameController };
