3d6d29fb3c0e262282d57dfaf3f11b3cab470ed4
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-02-15 15:58:51 +0100

    Update the file

--- src/driverModule.js
+++ src/driverModule.js
@@ -1,8 +1,15 @@
-import { Player } from './classes/player.js';
-import { createBoard } from './helperFunctions/generateBoard.js';
+import { createBoard } from './helperFunctions/createBoard.js';
+import { handleUserPlacement } from './helperFunctions/handleUserPlacement.js';
+import { GameController } from './classes/gameController.js';
 
+const rotateShipBtn = document.querySelector('#rotateShip');
 const userGrid = document.getElementById('userGrid');
 const computerGrid = document.getElementById('computerGrid');
+
+let gameController = new GameController();
+
+userGrid.addEventListener('click', (e) => handleUserPlacement(e, gameController));
+rotateShipBtn.addEventListener('click', () => gameController.rotateShip());
 
 export function driverModule() {
   createBoard(userGrid);
