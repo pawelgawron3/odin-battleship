3d1e64483937042025f79974bee58078314eef38
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-02-15 17:42:50 +0100

    Render the attacks on the board

---
+++ src/helperFunctions/renderAttacks.js
@@ -0,0 +1,20 @@
+export function renderAttacks(gameController, gridElement, playerType) {
+  const gameboard =
+    playerType === 'user' ? gameController.user.gameboard : gameController.computer.gameboard;
+
+  const cells = gridElement.querySelectorAll('.cell');
+
+  cells.forEach((cell) => {
+    const [row, col] = cell.dataset.index.split(',').map(Number);
+
+    if (gameboard.hits.some(([r, c]) => r === row && c === col)) {
+      cell.classList.add('hit');
+      cell.classList.remove('miss');
+    } else if (gameboard.misses.some(([r, c]) => r === row && c === col)) {
+      cell.classList.add('miss');
+      cell.classList.remove('hit');
+    } else {
+      cell.classList.remove('hit', 'miss');
+    }
+  });
+}
